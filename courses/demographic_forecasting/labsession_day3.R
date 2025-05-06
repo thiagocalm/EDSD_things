0c52e6637f2c6911911eb7364b1f01f94748fc8e
thiagocalm <thiagocalm@outlook.com>
2025-05-06 19:05:34 +0200

    trying to simulate a LC1 -- need to fix it

--- courses/demographic_forecasting/labsession_day3.R
+++ courses/demographic_forecasting/labsession_day3.R
@@ -288,7 +288,7 @@ df_plot <- fert |>
       )
   )
 
-# plotting it...
+# plotting log-ASFR
 
 df_plot |>
   filter(!is.infinite(logRates)) |>
@@ -298,3 +298,93 @@ df_plot |>
   facet_wrap(.~type) +
   theme_light() +
   scale_color_viridis_c(option = "D")
+
+# plotting TFR
+
+df_tfr <- df_plot |>
+  mutate(
+    ASFR = case_when(is.infinite(logRates) ~ 0, TRUE ~ exp(logRates))
+  ) |>
+  reframe(
+    TFR = sum(ASFR),
+    .by = c(year, type)
+  )
+
+ggplot() +
+  geom_line(
+    aes(
+      x = df_tfr$year[df_tfr$type == "Fitted"],
+      y = df_tfr$TFR[df_tfr$type == "Fitted"],
+      color = "Fitted"
+    ),
+    color = "orange2",
+    linewidth = 1.2
+  ) +
+  geom_point(
+    aes(
+      x = df_tfr$year[df_tfr$type != "Fitted"],
+      y = df_tfr$TFR[df_tfr$type != "Fitted"],
+      color = "Observed"
+    ),
+    color = "black",
+    size = 2
+  ) +
+  labs(
+    x = "Age",
+    y = "TFR"
+  ) +
+  theme_light()
+
+## Simulating fitted values using bootstrap
+
+## QUESTION: HOW DO WE SIMULATE A GLM???
+
+# # number of simulations
+# nS <- 100    ## increase for improved precision
+# set.seed(1)  ## for reproducibility
+# tF <- (max(fert$Year)+1):2050 # creating the rage of years
+# # object to store simulations
+# h = length(unique(age))
+# LFsim <- matrix(NA,h,nS)
+# s <- 1
+#
+# LFsim <- simulate(
+#   mod_LC1,
+#   nsim=nS,
+#   future=TRUE,
+#   bootstrap=TRUE
+# )
+#
+# for (s in 1:nS){
+#   lf.fore.sim <- simulate(
+#     mod_LC1,
+#     nsim=h,
+#     future=TRUE,
+#     bootstrap=TRUE
+#   )
+#   ## saving
+#   LFsim[,s] <- lf.fore.sim
+# }
+#
+# ## plotting all simulations
+# plot(t,y,ylim=range(y,LFsim),xlim=range(t,tF))
+# matlines(tF,LFsim,col="grey80",lty=1)
+#
+# ## deriving median and 95% PI
+# lev <- 95
+# lev.p <- lev/100
+# lf.fore.med <- apply(LFsim,1,median) # taking the median based on the rows of a matrix
+# lf.fore.low <- apply(LFsim,1,quantile,prob=(1-lev.p)/2) # taking the lower CI based on the rows of a matrix
+# lf.fore.up <- apply(LFsim,1,quantile,prob=1-(1-lev.p)/2) # taking the upper CI based on the rows of a matrix
+#
+# ## comparing PIs
+# plot(t,y,ylim=range(y,fF.AR$lower,lf.fore.low),xlim=range(t,tF))
+# ## analytical
+# lines(tF,fF.AR$mean,col=3,lwd=2)
+# lines(tF,fF.AR$upper[,2],col=3,lwd=2,lty=2)
+# lines(tF,fF.AR$lower[,2],col=3,lwd=2,lty=2)
+# ## simulations
+# lines(tF,lf.fore.med,col=4,lwd=2)
+# lines(tF,lf.fore.up,col=4,lwd=2,lty=2)
+# lines(tF,lf.fore.low,col=4,lwd=2,lty=2)
+# legend("bottomleft",c("analytical","simulations"),col=c(3,4),lwd=2)
